d985a764ee7994263b4a3888c5dea409e93b7926
Mani Bala <talk2manibala@gmail.com>
2023-03-04 21:50:47 +0800

    Updated

--- src/main/java/com/microservices/mstraining/controller/UserController.java
+++ src/main/java/com/microservices/mstraining/controller/UserController.java
@@ -5,6 +5,7 @@ import com.microservices.mstraining.service.UserNotFoundException;
 import com.microservices.mstraining.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,9 @@ import java.util.Locale;
 @RestController
 @RequestMapping("/api")
 public class UserController {
+
+//    @Value("${logging.level.org.springframework}")
+//    String loglevel;
 
     @Autowired
     UserService userService;
